7038faa471f3f8d5c158706f010e68c10e7dc888
Dilara Gilfanova <dgilfanova12@gmail.com>
2026-02-10 22:33:05 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// TODO: write your code in app.js
+import TraversingGoblin from './js/app.js';
+
+window.addEventListener('DOMContentLoaded', () => { //Запуск при загрузке страницы
+  new TraversingGoblin();
+});
+
---
+++ src/js/app.js
@@ -0,0 +1,72 @@
+// TODO: write code here
+import '../css/style.css';
+import goblinImage from '../img/goblin.png';
+
+export default class TraversingGoblin {
+  constructor(){// В конструкторе ИНИЦИАЛИЗИРУЕМ свойства    
+    this.totalCells = 16;
+    this.currentPosition = null;
+
+    this.gameInterval = null;
+    this.isGameRunning = false;
+    
+    this.init();// ВЫЗЫВАЕМ метод инициализации у этого экземпляра
+  }
+
+  init(){// ОПРЕДЕЛЯЕМ метод для класса
+    this.createBoard();// Создаём поле
+    this.createGoblin();// Создаём гоблина
+    this.startGame();
+  }
+
+  createBoard(){
+    const gameBoard = document.querySelector('.game-board');// Находим контейнер
+    gameBoard.innerHTML = ''; // Очищаем (на случай перезапуска)
+
+    for(let i = 0; i < this.totalCells; i++){
+      const cell = document.createElement('div');
+      cell.classList.add('cell');//Добавляем класс для ячейки - будет: class="cell"
+      cell.dataset.id = i;//Добавляем data-атрибут для этой же ячейки - будет: data-id="0"
+      gameBoard.appendChild(cell);//Добавляем узел в DOM в конец списка дочерних
+    }
+  }
+
+  createGoblin(){
+    const oldGoblin = document.querySelector('.goblin');
+    if (oldGoblin) { // Удаляем старого гнома (при сбросе игры)
+      oldGoblin.remove();
+    }
+
+    this.goblin = document.createElement('img');
+    this.goblin.classList.add('goblin');
+    this.goblin.src = goblinImage;
+  }
+
+  moveGoblinRandomly(){
+    let newPosition;
+    do {//цикл сначала выполняет код, потом проверяет условие
+      newPosition = Math.floor(Math.random()*this.totalCells);//случайная позиция из 16 клеток, округляется вниз до меньшего - случайно сгенерированное
+    } while (newPosition === this.currentPosition)//цикл повторяется, если 
+
+    this.currentPosition = newPosition;//новая позиция
+
+    const targetCell = document.querySelector(`.cell[data-id='${this.currentPosition}']`); //ищем клетку по data-id="this.currentPosition"
+    
+    targetCell.appendChild(this.goblin);// Перемещаем гнома
+  }
+
+  startGame(){
+    if (this.isGameRunning) {
+      return;
+    }
+
+    this.isGameRunning = true; 
+
+    this.moveGoblinRandomly();  // ← Первое появление сразу, потом каждые 2 секунды
+
+    this.gameInterval = setInterval (() => {// Таймер: каждые 2 секунды перемещаем гнома
+      this.moveGoblinRandomly();
+    }, 2000);
+  }
+};
+
